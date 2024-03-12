5a2c73dbca02f90481e8f568cebdb02fd7df35cf
Naman Rathore <108014558+namansingh98@users.noreply.github.com>
2024-03-12 11:16:00 +0000

    fixes

--- src/Providers/data/fetchWrapper.ts
+++ src/Providers/data/fetchWrapper.ts
@@ -1,43 +1,56 @@
-
 import { GraphQLFormattedError } from "graphql";
 
-
 type Error = {
-    message:string;
-    statusCode:string;
-}
-const customFetch = (url: string, options: RequestInit) => {const accessToken=localStorage.getItem("accessToken");
-
-const headers = options.headers as Record<string, string>;
-return await fetch(url,{
-    ...options,
-    headers: {
-        ...headers,
-        Authorization: headers.Authorization || `Bearer ${accessToken}`,
-    "Content-Type" : "application/json" ,
-    'Apollo-Require-Preflight': 'true' },
-})
+    message: string;
+    statusCode: string;
 }
 
-const getGraphQLErrors = (body:Record<"errors", GraphQLFormattedError[] | undefined>);
-Error | null => {
-     if (!body) {
-        return {
-            message:"unknown error",
-            statusCode:"Internal_Server_Error",
+const customFetch = async (url: string, options: RequestInit) => {
+    const accessToken = localStorage.getItem("accessToken");
+    const headers = options.headers as Record<string, string>;
+    return await fetch(url, {
+        ...options,
+        headers: {
+            ...headers,
+            Authorization: headers.Authorization || `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+            'Apollo-Require-Preflight': 'true'
         }
-     }
+    });
+}
 
+const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>): Error | null => {
+    if (!body) {
+        return {
+            message: "unknown error",
+            statusCode: "Internal_Server_Error",
+        }
+    }
 
-     if ("errors" in body) {
+    if ("errors" in body && body.errors) {
         const errors = body.errors;
+        const messages = errors.map(error => error?.message).join("");
+        const code = errors[0]?.extensions?.code;
+        return {
+            message: messages || JSON.stringify(errors),
+            statusCode: code || "500"
+        }
+    }
 
-        const messages=errors.map((error ) => error?.message?.join(""));
+    return null;
+}
 
-        const code=errors[0]?.extensions?.code;
-        return{message:messages || JSON.stringify(errors),
-        statusCode:code || 500}
-     }
 
-     return null;
+export const fetchwarpper =async (url:string,options:RequestInit) => {
+
+    const response =await customFetch(url,options)
+
+    const responseClone =response.clone();
+    const body =await responseClone.json();
+    const error =getGraphQLErrors(body);
+
+    if(error){
+        throw error
+    }
+    return response
 }
--- src/Providers/data/index.tsx
+++ src/Providers/data/index.tsx
@@ -1,9 +1,11 @@
 import { GraphQLClient } from "@refinedev/nestjs-query";
 export const API_URL = "https://api.crm.refine.dev";
+import { fetchwarpper } from "./fetchWrapper";
+
 export const client = new GraphQLClient(API_URL,{
 fetch: (url: string, options: RequestInit)=> {
     try{
-return fetchWrapper(url,options);
+return fetchwarpper(url,options);
     }catch (error){
         return Promise.reject(error as Error);
     }
